f8dd7a225f73e66d5f3bac3ab984a23923da047c
imgem52 <109936986+imgem52@users.noreply.github.com>
2022-11-29 11:04:01 +0530

    Add files via upload

---
+++ strings+ list manipulation_28Nov.py
@@ -0,0 +1,305 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+a="My name is Murtuza"
+print(a)
+
+
+# In[4]:
+
+
+a='My name is Murtuza'
+print(a)
+
+
+# In[6]:
+
+
+a="I have teacher's who name is Murtuza, teaching me Python"
+print(a)
+
+
+# In[7]:
+
+
+#is string iterable
+
+
+# In[44]:
+
+
+a='My name is Murtuza'
+print(a)
+
+
+# In[43]:
+
+
+range(a)
+
+
+# In[38]:
+
+
+range(10)
+
+
+# In[55]:
+
+
+a="Murtuza"
+
+
+# In[56]:
+
+
+len(a)
+
+
+# In[57]:
+
+
+for i in a:
+    print(i,end="")
+
+
+# In[59]:
+
+
+for i in range(len(a)):
+    print(a[i],end="")
+
+
+# In[60]:
+
+
+a
+
+# SLICING
+# In[85]:
+
+
+a="I have teacher's who name is Murtuza, teaching me Python"
+a
+
+
+# In[83]:
+
+
+a="name"
+
+
+# In[84]:
+
+
+a[1:3]
+
+
+# In[ ]:
+
+
+
+
+
+# In[75]:
+
+
+len(a)     # number of characters 
+
+
+# In[77]:
+
+
+a[0]      # it is starting from zero index
+
+
+# In[81]:
+
+
+a[55]
+
+
+# In[82]:
+
+
+a[:56] # before semicolon if nothing is there by default it will take zero
+
+
+# In[87]:
+
+
+a[0:56]    # forward slicing
+
+
+# In[92]:
+
+
+a[0:56:3]    # step size(jump activity)
+
+
+# In[95]:
+
+
+a[100]
+
+
+# In[101]:
+
+
+a[0:6]
+
+
+# In[105]:
+
+
+a[::3]   # by default entire str is printed with 2 semicolons step size
+
+
+# In[104]:
+
+
+a[0:56:3]
+
+
+# In[106]:
+
+
+a
+
+
+# In[108]:
+
+
+a[-6:-2]
+
+
+# In[110]:
+
+
+a[-1]
+
+
+# In[113]:
+
+
+a[-5:-1]
+
+
+# In[116]:
+
+
+a[-28:-2]
+
+
+# In[118]:
+
+
+a[-28:-2:-2]
+
+
+# In[132]:
+
+
+a="I love India"
+
+
+# In[123]:
+
+
+a[::-1]   # reverse the string using slicing operations
+
+
+# In[143]:
+
+
+c='india'
+c
+
+
+# In[144]:
+
+
+c.capitalize()
+
+
+# In[163]:
+
+
+c.center(50,"*")
+
+
+# In[150]:
+
+
+c.count("i")
+
+
+# In[169]:
+
+
+c= 'loveIndia'
+
+
+# In[172]:
+
+
+c.isupper()
+
+
+# In[152]:
+
+
+c.find("i")
+
+
+# In[153]:
+
+
+a=[1,2]
+if a
+
+
+# In[ ]:
+
+
+st="I am going to be master in Python"
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[133]:
+
+
+a.capitalize()
+
+
+# In[129]:
+
+
+b=[2,3,4]
+
+
+# In[130]:
+
+
+type(b)
+
